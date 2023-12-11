66d0b41a690f60705a7120b5aef5ac93ae5b9556
FacundoHernandez122 <138889551+FacundoHernandez122@users.noreply.github.com>
2023-12-10 22:30:57 -0300

    primer pusheo para actualizar el remoto

---
+++ src/controllers/searchReposController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { Octokit } from '@octokit/rest';
+
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN || "",
+});
+
+export async function searchRepos(req: Request, res: Response) {
+  const { repositoryName } = req.query;
+
+  try {
+    if (!repositoryName || repositoryName?.length === 0) {
+      return res.status(400).json({ error: "Query parameter is required" });
+    }
+
+    const { data: repos } = await octokit.rest.search.repos({
+      q: repositoryName as any,
+    });
+    res.json(repos);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
---
+++ src/routes/githubSearchRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { getReposByUsername } from "../controllers/reposController";
+import { searchRepos } from "../controllers/searchReposController";
+import { searchUsers } from "../controllers/searchUsersController";
+
+const router = express.Router();
+
+router.get("/repos/:username", getReposByUsername);
+router.get("/search/repos", searchRepos);
+router.get("/search/users", searchUsers);
+
+export default router;
